f341663d7b0d992c181925315c94e42b9b78da34
Lean1818 <leaeaean@gmail.com>
2016-10-27 22:04:32 +0800

    updated

--- Exercise 4/mypage.php
+++ Exercise 4/mypage.php
@@ -21,6 +21,14 @@ td, th {
 tr:nth-child(even) {
     background-color: #000000;
 }
+
+#myform {
+	color: white;
+}
+
+.error {
+	color : red;
+}
 </style>
 </head>
 <body bgcolor="#0E3468">
@@ -143,5 +151,124 @@ tr:nth-child(even) {
 <font size="36">Course: BSIT-MI<br></font>
 </p>
 <br>
+
+<div id="phpform">
+<?php	
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $nickErr = "";
+$name = $email = $gender = $comment = $nick = $home = $num = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed"; 
+    }
+  }
+  
+  if (empty($_POST["home"])) {
+    $home = "";
+  } else {
+    $home = test_input($_POST["home"]);
+  }
+  
+  if (empty($_POST["num"])){
+	  $num = "";
+  } else {
+	  $num = test_input($_POST["num"]);
+  }
+  
+  if (empty($_POST["nick"])) {
+	  $nickErr = "Nickname is required";
+  } else {
+	  $nick = test_input($_POST["nick"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nickErr = "Only letters and white space allowed"; 
+    }
+  }
+  
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; 
+    }
+  }
+  
+
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+  
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+<div id="myform">
+<h2>Fill Up the form</h2>
+<p><span class="error">* required field.</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Complete Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  Nickname: <input type="text" name="nick" value="<?php echo $nick;?>">
+  <span class="error">* <?php echo $nickErr;?></span>
+  <br><br>
+  Email Address: <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error"><?php echo $emailErr;?></span>
+  <br><br>
+  Home Address: <input type="text" name="home" value="<?php echo $home;?>">
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  Cellphone Number: <input type="number" name="num" value="<?php echo $num;?>">
+  <br><br>
+  Comment: <br><textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+
+</form>
+<?php
+echo "<h2>Results:</h2>";
+echo $name;
+echo "<br>";
+echo $nick;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $home;
+echo "<br>";
+echo $gender;
+echo "<br>";
+echo $num;
+echo "<br>";
+echo $comment;
+echo "<br>";
+
+?>
+</div>
 </body>
 </html>
