79d4f65747c480b6fd0e78d6d887f517c215859f
Lean1818 <leaeaean@gmail.com>
2016-11-17 08:57:19 +0800

    edited main page button

--- exercise6/codeigniter/application/views/Users_add.php
+++ exercise6/codeigniter/application/views/Users_add.php
@@ -19,7 +19,7 @@
 <br>
 <br>
 <tr>
-    <td align="center"><a href="<?php echo base_url(); ?>index.php/users">back to main page</a></td>
+    <td align="center"><a href="<?php echo base_url(); ?>index.php/users"><u>back to main page</u></a></td>
     </tr>
     <br>
     <br>
