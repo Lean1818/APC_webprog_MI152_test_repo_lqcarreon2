c9aaeb337c51f0fe8361aa8e5cd15ef55e3cc361
Lean1818 <leaeaean@gmail.com>
2016-11-24 09:10:19 +0800

    href on add data

--- Exercise 5/index.php
+++ Exercise 5/index.php
@@ -78,6 +78,7 @@ function delete_id(id)
 	<th>Comment</th>
     <th colspan="2">Operations</th>
     </tr>
+	<a href="mypage.php">Back to Main page</a>
     <?php
  $sql_query="SELECT * FROM users";
  $result_set=mysql_query($sql_query);
