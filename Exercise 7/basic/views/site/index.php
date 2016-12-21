cb6a7fd1105172e8582fba8e0cd3dd3b3005f792
Lean1818 <leaeaean@gmail.com>
2016-12-22 00:17:31 +0800

    removed heaeder

---
+++ Exercise 7/basic/views/site/index.php
@@ -0,0 +1,175 @@
+
+?>
+<!DOCTYPE html> 
+<html lang = "en">
+
+<style>
+ul
+	{
+    list-style-type:none;
+	}
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 50%;	
+    color: black;
+}
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+tr:nth-child(even) {
+    background-color: black;
+}
+
+#myform {
+	color: white;
+}
+
+.error {
+	color : red;
+}
+
+a{
+	color: white;
+	text-decoration: none;
+}
+
+a:hover {
+	color: red;
+}
+
+.Trivia {
+color: black;
+}
+</style>
+ 
+ 
+
+	<title>My Home Page</title>
+ 
+	<body bgcolor="#0E3468">
+<A NAME="Home">
+<center>
+<center>
+<br>
+<center>
+<h1><a href = "index.php?r=users%2Findex">Create Database</a></h1>
+<h1><font color="White">Lean Nasser Q. Carreon </h1>
+</center>
+<center>
+<nav class="nav">
+<ul id="navmenu">
+<table width="500">
+  <tr>
+    <th> <button type="button" onclick="document.getElementById('Favorite Websites').style.display='table-cell'">Favorite Websites</button></th>
+    <th><button type="button" onclick="document.getElementById('Hobbies').style.display='table-cell'">Hobbies</button></th>
+    <th><button type="button" onclick="document.getElementById('Interests').style.display='table-cell'">Interest</button></th>
+  </tr>
+  <tr>
+    <td id="Favorite Websites" style="display:none"><a href="#Favorite Website"><font color="White">Facebook, Twitter, Moodle</td>
+    <td id="Hobbies" style="display:none"><a href="#Hobby"><font color="White">Basketball, Working out, Dota 2</td>
+    <td id="Interests" style="display:none"><a href="#Interest"><font color="White">Cars, Shoes, Laptops</td>
+  </tr>
+</table>
+  <a href="#Home"><font color="White">Home</a>
+  <a href="#About Me"><font color="White">About Me</a>
+  <p><b>Tip:</b> Underlined words are pressable</p>
+ 
+
+</ul>
+</nav>
+</center>
+	Trivia about my Interest/Hobbies or myself
+ <p>Click for a Trivia.</p>
+<button class = "Trivia" type="button" onclick="document.getElementById('Trivia').innerHTML = 'I  always play Basketball!'">Click For A Trivia!</button>
+<button class = "Trivia"  type="button" onclick="document.getElementById('Trivia').innerHTML = 'I  always play Dota 2'">Click For A Trivia!</button>
+<button class = "Trivia"  type="button" onclick="document.getElementById('Trivia').innerHTML = 'I  always active in Facebook!'">Click For A Trivia!</button>
+<button class = "Trivia"  type="button" onclick="document.getElementById('Trivia').innerHTML = 'I  like shoes!'">Click For A Trivia!</button>
+<button class = "Trivia"  type="button" onclick="document.getElementById('Trivia').innerHTML = 'I  like cars!'">Click For A Trivia!</button>
+
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<A NAME ="Favorite Website" align="left">
+<div> <font size="36"><h1><big>FAVORITE WEBSITES</big></h1></font></div>
+<ul><li><a href="#Home"><font color="White" align="left" >Home</a></li></ul>
+<p align="left">
+<a href="https://twitter.com/LeanSanity123"><img src="uploads/Twitter.png"width=310px" height=310">
+</a>
+<a href="https://www.facebook.com/Wewertt"> <img src="uploads/Facebook.jpg"width=310px" height=310"> 
+</a>
+<a href="http://moodle2.apc.edu.ph/user/profile.php?id=8229"> <img src="uploads/Moodle.png">
+</a>
+</p>
+<br>
+<A NAME="Hobby"align="left">
+<div> <font size="36"><h1><big>HOBBIES</big></h1></font> </div>
+<ul><li><a href="#Home"><font color="White">Home</a></li></ul>
+<img src="uploads/Basketball.JPG"width=310px" height=410">
+<img src="uploads/Dota.jpg"width=510px" height=410">
+<img src="uploads/workout.jpg"width=330px" height=410">
+<br>
+<A NAME="Interest"align="left">
+<div> <font size="36"><h1><big>INTEREST</big></h1></font> </div>
+<ul><li><a href="#Home"><font color="White">Home</a></li></ul>
+<img src="uploads/car.JPG"width=410px" height=410">
+<img src="uploads/shoes.jpg"width=410px" height=410">
+<img src=" uploads/laptop.jpg"width=410px" height=410">
+<br>
+<A NAME="About Me"align="left">
+<div> <font size="36"><h1><big>About Me</big></h1></font> </div>
+<ul><li><a href="#Home"><font color="White">Home</a></li></ul>
+<p align="left>
+<br>
+<font size="36"><br></font>
+<font size="36">Full name: Lean Nasser Quiles Carreon<br></font>
+<font size="36">Nickname: Lee<br></font>
+<font size="36">Address: Taguig City<br></font>
+<font size="36">School: Asia Pacific College<br></font>
+<font size="36">Course: BSIT-MI<br></font>
+</p>
+<br>
+
+</body>
+	
+</html>
